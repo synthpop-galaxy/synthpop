7fe3d46f5e6c17ab499b6ab4301bf05ad68f8c91
jkluter <jonas.klueter@web.de>
2023-03-10 17:57:45 -0600

    include a setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-import os
-
-from setuptools import setup, find_packages, Command
+from setuptools import setup, find_packages
 
 setup(
     name='synthpop',
@@ -9,7 +7,6 @@ setup(
     author='J. Klüter, S. Johnson, M.J. Huston, A. Aronica, M. Penny',
     license='GPLv3',
     packages=find_packages(),
-
     include_package_data=True,
     install_requires=[
         'numpy>=1.23.2',
@@ -24,28 +21,4 @@ setup(
     },
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown'
-    cmdclass = {
-    'uninstall': UninstallCommand,
-    },
 )
-
-class UninstallCommand(Command):
-    description = 'Uninstall the package and remove any generated symbolic links.'
-    user_options = []
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        """Remove any generated symbolic links."""
-        # Remove file1.txt
-        if os.path.islink("modules"):
-            os.unlink(symlink)        # Remove file2.txt
-        if os.path.islink("models"):
-            os.unlink("models")
-        if os.path.islink("config_files"):
-            os.unlink("config_files")
-        if os.path.islink("constants.py"):
-            os.unlink('constants.py')
