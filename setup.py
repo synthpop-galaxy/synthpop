a208f339382b7e79cce3763b83b3fbccfee320c5
jkluter <jonas.klueter@web.de>
2023-03-13 16:25:55 -0500

    remove symbolic links when uninstalled

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-from setuptools import setup, find_packages
+import os
+
+from setuptools import setup, find_packages, Command
 
 setup(
     name='synthpop',
@@ -7,10 +9,7 @@ setup(
     author='J. Klüter, S. Johnson, M.J. Huston, A. Aronica, M. Penny',
     license='GPLv3',
     packages=find_packages(),
-    data_files=[('modules', [], {'is_symlink': True}),
-                ('models', [], {'is_symlink': True}),
-                ('config_files', [], {'is_symlink': True}),
-                ('constants.py', [], {'is_symlink': True})],
+
     include_package_data=True,
     install_requires=[
         'numpy>=1.23.2',
@@ -25,4 +24,28 @@ setup(
     },
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown'
+    cmdclass = {
+    'uninstall': UninstallCommand,
+    },
 )
+
+class UninstallCommand(Command):
+    description = 'Uninstall the package and remove any generated symbolic links.'
+    user_options = []
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        """Remove any generated symbolic links."""
+        # Remove file1.txt
+        if os.path.islink("modules"):
+            os.unlink(symlink)        # Remove file2.txt
+        if os.path.islink("models"):
+            os.unlink("models")
+        if os.path.islink("config_files"):
+            os.unlink("config_files")
+        if os.path.islink("constants.py"):
+            os.unlink('constants.py')
