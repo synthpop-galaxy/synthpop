35c0743d6c8d755097fb8b0fdc7dd387445f1116
Macy Huston <mhuston@berkeley.edu>
2024-07-03 13:40:53 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,8 @@ sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, os.path.abspath('../../synthpop/_modules/'))
 sys.path.insert(0, os.path.abspath('../../synthpop/_modules/age/'))
 
-python -m synthpop.migrate_interactive_part
+from synthpop import migrate_interactive_part
+
 
 project = 'SynthPop'
 copyright = 'tbd'
