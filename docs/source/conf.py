9c7f839de1e6d2f8adfee22c43be3bbb0a6c95c7
Macy Huston <mhuston@berkeley.edu>
2024-07-03 13:14:30 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,6 +5,9 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../synthpop/'))
+sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../synthpop/_modules/'))
 
 project = 'SynthPop'
 copyright = 'tbd'
