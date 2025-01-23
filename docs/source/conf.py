ac6391b55fead7dc8051d40997eeceff0ca4d3e5
Macy Huston <mhuston@berkeley.edu>
2025-01-23 10:36:17 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,6 +48,7 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_css_files = ["custom.css"]
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
