besancon kinematics with correct rotation curve + minor renaming

synthpop/modules/kinematics/besancon2003.py
@@ -211,7 +211,8 @@ class Besancon2003(Kinematics):
             dsigma_u_dr = self.disp_grad / 2 * sigma_u_r
             with warnings.catch_warnings():
                 warnings.filterwarnings("ignore", message="divide by zero encountered in multiply")
-                rho_component = np.nan_to_num(r / rho * drho_dr, nan=0.0)
+                warnings.filterwarnings("ignore", message="invalid value encountered in multiply")
+                rho_component = np.nan_to_num(r / rho * drho_dr, nan=0.0, posinf=0.0, meginf=0.0)
             V_ad = -sigma_u_r ** 2 / (2 * self.sun.v_lsr) * \
                 (rho_component
                  + 2 * r / sigma_u_r * dsigma_u_dr