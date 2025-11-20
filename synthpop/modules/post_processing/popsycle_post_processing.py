ead22bd9b826452a0248076e67b1577f61581358
Shep Brooke <shepbrooke@laser.astro.berkeley.edu>
2025-11-19 18:00:05 -0800

    Added binary systems to output for PopSyCLE

--- synthpop/modules/post_processing/popsycle_post_processing.py
+++ synthpop/modules/post_processing/popsycle_post_processing.py
@@ -4,7 +4,7 @@ ready to be plugged in at the calc_events stage.
 Note: obsmag must be set to FALSE in config file to use this module
 """
 
-__author__ = "M.J. Huston, A. Kim, C. Lee"
+__author__ = "M.J. Huston, A. Kim, C. Lee, S. Brooke"
 
 from ._post_processing import PostProcessing
 import time
@@ -24,38 +24,42 @@ filter_matching_mist = {'ubv_J': "UKIDSS_J",
                         'ubv_B': "Bessell_B",
                         'ubv_V': "Bessell_V",
                         'ubv_R': "Bessell_R"
-                       }
+                        }
 
 synthpop_nonmag_cols = ['l', 'b', 'Dist',
                         'x', 'y', 'z',
-                        'vr_bc', 'mul','mub',
+                        'vr_bc', 'mul', 'mub',
                         'U', 'V', 'W',
-                        'iMass','Mass',
+                        'iMass', 'Mass',
                         'log_L', 'log_g', 'log_Teff', '[Fe/H]', 'age',
                         'pop', 'phase'
                         ]
-synthpop_nonmag_bin_cols = []
+synthpop_nonmag_bin_cols = ['system_idx', 'zams_mass', 'Teff', 'L',
+                            'logg', 'isWR', 'mass', 'phase', 'metallicity',
+                            'log_a', 'e', 'i', 'Omega', 'omega', 'zams_mass_prim'
+                            ]
 
 popsycle_nonmag_cols = ['glat', 'glon', 'rad',
-                        'px', 'py', 'pz', 
-                        'vr', 'mu_lcosb', 'mu_b', 
-                        'vx', 'vy', 'vz', 
-                        'zams_mass', 'mass', 'systemMass', 
+                        'px', 'py', 'pz',
+                        'vr', 'mu_lcosb', 'mu_b',
+                        'vx', 'vy', 'vz',
+                        'zams_mass', 'mass', 'systemMass',
                         'mbol', 'grav', 'teff', 'feh', 'age',
                         'exbv', 'popid',
-                        'isMultiple', 'N_companions', 'rem_id', 'obj_id'] 
+                        'isMultiple', 'N_companions', 'rem_id', 'obj_id']
+
 
 class PopsyclePostProcessing(PostProcessing):
 
     def __init__(self, model, logger, bin_edges_number=None, filter_sets=['ubv'], **kwargs):
         super().__init__(model, logger, **kwargs)
         self.bin_edges_number = bin_edges_number
-        #self.filter_sets = filter_sets
+        # self.filter_sets = filter_sets
         self.mag_cols = []
         self.synthpop_mag_cols = []
         for fset in filter_sets:
-            self.mag_cols += [fset+'_'+f for f in filter_set_dict[fset]]
-            self.synthpop_mag_cols += [filter_matching_mist[fset+'_'+f] for f in filter_set_dict[fset]]
+            self.mag_cols += [fset + '_' + f for f in filter_set_dict[fset]]
+            self.synthpop_mag_cols += [filter_matching_mist[fset + '_' + f] for f in filter_set_dict[fset]]
 
     def do_post_processing(self, dataframe: pd.DataFrame) -> pd.DataFrame:
         """
@@ -64,60 +68,51 @@ class PopsyclePostProcessing(PostProcessing):
         """
         self.logger.info(f"Beginning PopSyCLE postprocessing.")
 
-        # Drop unused data
-        cols_to_cut = []
-        for col in dataframe.keys():
-            if col not in (synthpop_nonmag_cols+self.synthpop_mag_cols+synthpop_nonmag_bin_cols+
-                            [self.model.populations[0].extinction.A_or_E_type]):
-                cols_to_cut.append(col)
-        dataframe.drop(columns=cols_to_cut, inplace=True)
-        self.logger.info("Unused columns dropped")
-
         self.output_root = f"{self.model.get_filename(self.model.l_deg, self.model.b_deg, self.model.solid_angle)}_psc"
-        
+
         # Translate extinction to Ebv extinction
-        if not self.model.populations[0].extinction.A_or_E_type=="E(B-V)":
+        if not self.model.populations[0].extinction.A_or_E_type == "E(B-V)":
             extinction = self.model.populations[0].extinction
             extinction_type = self.model.populations[0].extinction.A_or_E_type
-            dataframe.rename(columns={extinction_type:'ext_orig'}, inplace=True)
-            #ext_in_map = dataframe[extinction_type].to_numpy()
+            dataframe.rename(columns={extinction_type: 'ext_orig'}, inplace=True)
+            # ext_in_map = dataframe[extinction_type].to_numpy()
             Av_Aref = extinction.extinction_at_lambda(0.544579, 1.0)
             Ab_Aref = extinction.extinction_at_lambda(0.438074, 1.0)
             pd.eval("exbv = (Ab_Aref - Av_Aref)*dataframe.ext_orig", target=dataframe, inplace=True)
             dataframe.drop(columns=['ext_orig'], inplace=True)
             self.logger.info("E(B-V) calculated")
         else:
-            dataframe.rename(columns={"E(B-V)":'exbv'}, inplace=True)
-        
+            dataframe.rename(columns={"E(B-V)": 'exbv'}, inplace=True)
+
         # create log (with same info as galaxia log)
-        #dtype = [('latitude', 'f8'), ('longitude', 'f8'), ('surveyArea', 'f8')]
-        #log = np.zeros(1, dtype=dtype)
+        # dtype = [('latitude', 'f8'), ('longitude', 'f8'), ('surveyArea', 'f8')]
+        # log = np.zeros(1, dtype=dtype)
         latitude = self.model.l_deg
         longitude = self.model.b_deg
         surveyArea = self.model.solid_angle
-        if self.model.solid_angle_unit=='sr':
-            surveyArea *= (180/np.pi)**2
-            
+        if self.model.solid_angle_unit == 'sr':
+            surveyArea *= (180 / np.pi) ** 2
+
         os.makedirs('/'.join(self.output_root.split('/')[:-1]), exist_ok=True)
         with open(self.output_root + '_synthpop_params.txt', 'w') as params_file:
             params_file.write(f"seed {self.model.parms.random_seed}\n")
         self.logger.info("Parameter file written")
 
-        dataframe.rename(columns={'iMass': 'zams_mass','Mass': 'mass', 
-                                 'x': 'px', 'y': 'py', 'z': 'pz',
-                                 'U': 'vx', 'V': 'vy', 'W': 'vz',
-                                 'vr_bc': 'vr', 'mul': 'mu_lcosb', 'mub': 'mu_b',
-                                 'pop': 'popid',
-                                 'b': 'glat', 'l': 'glon', 'Dist': 'rad',
-                                 'log_g': 'grav', 'log_Teff': 'teff', '[Fe/H]': 'feh'}, 
+        dataframe.rename(columns={'iMass': 'zams_mass', 'Mass': 'mass',
+                                  'x': 'px', 'y': 'py', 'z': 'pz',
+                                  'U': 'vx', 'V': 'vy', 'W': 'vz',
+                                  'vr_bc': 'vr', 'mul': 'mu_lcosb', 'mub': 'mu_b',
+                                  'pop': 'popid',
+                                  'b': 'glat', 'l': 'glon', 'Dist': 'rad',
+                                  'log_g': 'grav', 'log_Teff': 'teff', '[Fe/H]': 'feh'},
                          inplace=True)
         self.logger.info("Basic columns renamed")
         pd.eval('age = log10(dataframe.age*1e9)', target=dataframe, inplace=True)
-        #dataframe.loc[:,'age'] = np.log10(dataframe['age']*1e9)
+        # dataframe.loc[:,'age'] = np.log10(dataframe['age']*1e9)
         self.logger.info("Age units converted")
 
-        #wrap_idx = dataframe[dataframe['glon'] > 180].index
-        #dataframe.loc[wrap_idx, 'glon'] -= 360
+        # wrap_idx = dataframe[dataframe['glon'] > 180].index
+        # dataframe.loc[wrap_idx, 'glon'] -= 360
         pd.eval('glon = dataframe.glon - (dataframe.glon>180)*360', target=dataframe, inplace=True)
         self.logger.info("Galactic longitude wrapped")
         pd.eval("mbol = -2.5 * dataframe.log_L + 4.75", target=dataframe, inplace=True)
@@ -129,26 +124,134 @@ class PopsyclePostProcessing(PostProcessing):
         pd.eval("systemMass = dataframe.mass", target=dataframe, inplace=True)
         self.logger.info("Added systemMass via eval")
 
-
-        dataframe.rename(columns={filter_matching_mist[f]:f for f in self.mag_cols},
+        dataframe.rename(columns={filter_matching_mist[f]: f for f in self.mag_cols},
                          inplace=True)
         self.logger.info("Renamed mag cols")
-        
-        dataframe.loc[:, 'isMultiple'] = np.zeros(dataframe.shape[0], dtype=int)
-        dataframe.loc[:, 'N_companions'] = np.zeros(dataframe.shape[0], dtype=int)
+
         phases = np.nan_to_num(dataframe['phase'].to_numpy())
-        dataframe.loc[:, 'rem_id'] = (phases*(phases>100)).astype(int)
+        dataframe.drop(columns=['phase'], inplace=True)
+        dataframe.loc[:, 'rem_id'] = (phases * (phases > 100)).astype(int)
         dataframe.loc[:, 'obj_id'] = np.arange(0, len(dataframe))
         self.logger.info("Multiplicity and remnant columns added")
 
-        self.logger.info("PopSyCLE dataframe modifications complete - binning and saving file")
         _, lat_bin_edges, long_bin_edges = _get_bin_edges(latitude, longitude, surveyArea, self.bin_edges_number)
+
+        if "Is_Binary" in dataframe.columns:
+            if dataframe["Is_Binary"].any():
+
+                synthpop_nonmag_bin_cols = ['system_idx', 'zams_mass', 'Teff', 'L',
+                                            'logg', 'isWR', 'mass', 'phase', 'metallicity',
+                                            'log_a', 'e', 'i', 'Omega', 'omega', 'zams_mass_prim']
+
+                dataframe.rename(columns={'Is_Binary': 'isMultiple', 'combined_logP': 'logP'}, inplace=True)
+
+                counts_1 = dataframe.groupby("primary_ID")["primary_ID"].transform('count')
+
+                id_counts = dataframe['primary_ID'].value_counts()
+                counts_2 = dataframe['ID'].map(id_counts)
+
+                dataframe['N_companions'] = counts_2.add(counts_1, fill_value=0)
+                dataframe['N_companions'] = dataframe['N_companions'].fillna(0)
+
+                dataframe["temp_id"] = dataframe["ID"]
+                dataframe["temp_primary"] = dataframe["primary_ID"]
+
+                temp_df = dataframe[['temp_id', 'obj_id']].copy()
+                temp_df.rename(columns={'temp_id': 'temp_primary'}, inplace=True)
+                merged_stars = dataframe.merge(temp_df, on='temp_primary', how='left', suffixes=('', '_new'))
+                dataframe['system_idx'] = merged_stars['obj_id_new']
+
+                temp_df = dataframe[['temp_id', 'zams_mass']].copy()
+                temp_df.rename(columns={'temp_id': 'temp_primary'}, inplace=True)
+                merged_stars = dataframe.merge(temp_df, on='temp_primary', how='left', suffixes=('', '_new'))
+                dataframe['zams_mass_prim'] = merged_stars['zams_mass_new']
+
+                dataframe.drop(columns=['temp_id', "temp_primary"], inplace=True)
+
+                binary_dict = dataframe[~dataframe["isMultiple"].isin([0, 1])]
+                binary_dict = binary_dict.reset_index(drop=True)
+                dataframe = dataframe[dataframe["isMultiple"].isin([0, 1])]
+                dataframe = dataframe.reset_index(drop=True)
+
+                binary_dict.rename(columns={"feh": "metallicity", "grav": "logg", "rem_id": "phase"}, inplace=True)
+                binary_dict["isWR"] = np.zeros(binary_dict.shape[0], dtype=int)
+                combined_mass = binary_dict["zams_mass"] + binary_dict["zams_mass_prim"]
+                semimajor_axis = np.cbrt((10 ** (binary_dict["logP"])) ** 2 * combined_mass)
+                binary_dict.drop(columns=['logP'], inplace=True)
+
+                binary_dict["log_a"] = np.log10(semimajor_axis)
+                binary_dict["e"] = np.zeros(binary_dict.shape[0], dtype=int)
+                x = np.random.uniform(0, 1, size=binary_dict.shape[0])
+                y = np.random.uniform(0, 1, size=binary_dict.shape[0])
+                z = np.random.uniform(0, 1, size=binary_dict.shape[0])
+                binary_dict["i"] = np.arccos(x) * 180 / np.pi
+                binary_dict["Omega"] = 360 * y
+                binary_dict["omega"] = 360 * z
+
+                synthpop_bin_mag_cols = []
+
+                filters_ukirt = {"H", "J", "K"}
+                filters_ubv = {"U", "B", "V", "R", "I"}
+                for filter in filters_ukirt:
+                    if f"ubv_{filter}" in binary_dict.columns:
+                        binary_dict.rename(columns={f"ubv_{filter}": f"m_ukirt_{filter}"}, inplace=True)
+                    else:
+                        binary_dict[f"m_ukirt_{filter}"] = np.nan
+                    synthpop_bin_mag_cols.append(f"m_ukirt_{filter}")
+
+                for filter in filters_ubv:
+                    if f"ubv_{filter}" in binary_dict.columns:
+                        binary_dict.rename(columns={f"ubv_{filter}": f"m_ubv_{filter}"}, inplace=True)
+                    else:
+                        binary_dict[f"m_ubv_{filter}"] = np.nan
+                    synthpop_bin_mag_cols.append(f"m_ubv_{filter}")
+
+                cols_to_cut = []
+                for col in binary_dict.keys():
+                    if col not in (synthpop_nonmag_bin_cols + synthpop_bin_mag_cols + popsycle_nonmag_cols):
+                        cols_to_cut.append(col)
+                binary_dict.drop(columns=cols_to_cut, inplace=True)
+
+                binary_dict = binary_dict.reset_index(drop=True)
+                dataframe = dataframe.reset_index(drop=True)
+
+                h5file = h5py.File(f"{self.output_root}_companions.h5", 'w')
+                h5file['lat_bin_edges'] = lat_bin_edges
+                h5file['long_bin_edges'] = long_bin_edges
+                h5file.close()
+
+                _bin_lb_hdf5(lat_bin_edges, long_bin_edges, binary_dict, f"{self.output_root}_companions")
+                print(f"PopSyCLE formatted companions output saved in {self.output_root}_companions.h5")
+
+                self.logger.info("Binary file generated.")
+
+            else:
+                dataframe.loc[:, 'isMultiple'] = np.zeros(dataframe.shape[0], dtype=int)
+                dataframe.loc[:, 'N_companions'] = np.zeros(dataframe.shape[0], dtype=int)
         
+        # Drop unused data
+        cols_to_cut = []
+        for col in dataframe.keys():
+            if col not in (synthpop_nonmag_cols + self.synthpop_mag_cols +
+                           [self.model.populations[
+                                0].extinction.A_or_E_type] + popsycle_nonmag_cols + self.mag_cols):
+                cols_to_cut.append(col)
+        dataframe.drop(columns=cols_to_cut, inplace=True)
+        self.logger.info("Unused columns dropped")
+
+        cols_to_cut = []
+        for col in dataframe.keys():
+            if col not in (popsycle_nonmag_cols + self.mag_cols):
+                cols_to_cut.append(col)
+        new_data = dataframe.drop(columns=cols_to_cut)
+
+        self.logger.info("PopSyCLE dataframe modifications complete - binning and saving file")
+
         with h5py.File(f"{self.output_root}.h5", 'w') as h5file:
             h5file['lat_bin_edges'] = lat_bin_edges
             h5file['long_bin_edges'] = long_bin_edges
-        
-        _bin_lb_hdf5(lat_bin_edges, long_bin_edges, dataframe[popsycle_nonmag_cols+self.mag_cols], self.output_root)
+
+        _bin_lb_hdf5(lat_bin_edges, long_bin_edges, dataframe, self.output_root)
         self.logger.info(f"PopSyCLE formatted output saved in {self.output_root}.h5")
-    
+
         return dataframe
