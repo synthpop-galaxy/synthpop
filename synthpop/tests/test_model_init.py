62af753658ab8876c2c90cb376ffb4398d7404fe
Macy Huston <mhuston@berkeley.edu>
2026-08-28 14:44:02 -0700

    model init test module

---
+++ synthpop/tests/test_model_init.py
@@ -0,0 +1,70 @@
+import synthpop
+
+# Make sure we can initialize all the existing models
+# Swap in a faster-loading extinction map in place of default
+
+def test_init_besancon_Robin2003():
+    mod = synthpop.SynthPop(model_name='besancon_Robin2003',
+                         extinction_map_kwargs={"name":"maps_from_dustmaps", "dustmap_name":"marshall"},
+                        )
+    mod.init_populations()
+    
+def test_init_galaxia_Sharma2011():
+    mod = synthpop.SynthPop(model_name='galaxia_Sharma2011',
+                         extinction_map_kwargs={"name":"maps_from_dustmaps", "dustmap_name":"marshall"},
+                        )
+    mod.init_populations()
+    
+def test_init_genstars_Koshimoto2021():
+    mod = synthpop.SynthPop(model_name='genstars_Koshimoto2021',
+                         extinction_map_kwargs={"name":"maps_from_dustmaps", "dustmap_name":"marshall"},
+                        )
+    mod.init_populations()
+    
+def test_init_genstars_Koshimoto2022():
+    mod = synthpop.SynthPop(model_name='genstars_Koshimoto2022',
+                         extinction_map_kwargs={"name":"maps_from_dustmaps", "dustmap_name":"marshall"},
+                        )
+    mod.init_populations()
+    
+def test_init_GUMS_dr3():
+    mod = synthpop.SynthPop(model_name='GUMS_dr3',
+                         extinction_map_kwargs={"name":"maps_from_dustmaps", "dustmap_name":"marshall"},
+                        )
+    mod.init_populations()
+    
+def test_init_GUMS_dr3_mod_dens():
+    mod = synthpop.SynthPop(model_name='GUMS_dr3_mod_dens',
+                         extinction_map_kwargs={"name":"maps_from_dustmaps", "dustmap_name":"marshall"},
+                        )
+    mod.init_populations()
+    
+def test_init_Huston2024():
+    mod = synthpop.SynthPop(model_name='Huston2024',
+                         extinction_map_kwargs={"name":"maps_from_dustmaps", "dustmap_name":"marshall"},
+                        )
+    mod.init_populations()
+    
+def test_init_Huston2025():
+    mod = synthpop.SynthPop(model_name='Huston2025',
+                         extinction_map_kwargs={"name":"maps_from_dustmaps", "dustmap_name":"marshall"},
+                        )
+    mod.init_populations()
+    
+def test_init_Huston2026():
+    mod = synthpop.SynthPop(model_name='Huston2026',
+                         extinction_map_kwargs={"name":"maps_from_dustmaps", "dustmap_name":"marshall"},
+                        )
+    mod.init_populations()
+
+def test_init_validation_model():
+    mod = synthpop.SynthPop(model_name='validation_model',
+                         extinction_map_kwargs={"name":"maps_from_dustmaps", "dustmap_name":"marshall"},
+                        )
+    mod.init_populations()
+    
+def test_init_Vasiliev2026():
+    mod = synthpop.SynthPop(model_name='Vasiliev2026',
+                         extinction_map_kwargs={"name":"maps_from_dustmaps", "dustmap_name":"marshall"},
+                        )
+    mod.init_populations()
